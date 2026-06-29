7cfc957b5342eff8f8b3ee33cbd97404eff5da48
Mike Felder <2172656+mikefelder@users.noreply.github.com>
2026-06-29 16:04:23 -0500

    Add 2014 Free Press Summer Festival data and layout
    - Created snapshot JSON file for the 2014 Free Press Summer Festival, including event details, navigation, social links, sponsors, and footer information.
    - Implemented a new layout component for the 2014 festival, utilizing the snapshot data for dynamic content rendering.
    - Developed the main index page for the 2014 festival, featuring video content, announcements, and social media links.
    - Updated the main index page to reflect the 2014 festival as live, with appropriate links and status.

---
+++ public/shared-assets/2014/assets/js/local.js
@@ -0,0 +1,42 @@
+var _____WB$wombat$assign$function_____=function(name){return (globalThis._wb_wombat && globalThis._wb_wombat.local_init && globalThis._wb_wombat.local_init(name))||globalThis[name];};if(!globalThis.__WB_pmw){globalThis.__WB_pmw=function(obj){this.__WB_source=obj;return this;}}{
+let window = _____WB$wombat$assign$function_____("window");
+let self = _____WB$wombat$assign$function_____("self");
+let document = _____WB$wombat$assign$function_____("document");
+let location = _____WB$wombat$assign$function_____("location");
+let top = _____WB$wombat$assign$function_____("top");
+let parent = _____WB$wombat$assign$function_____("parent");
+let frames = _____WB$wombat$assign$function_____("frames");
+let opener = _____WB$wombat$assign$function_____("opener");
+$(function() {
+    $(".rslides").responsiveSlides();
+    $(".reveal").click(function(){
+	    $(this).parent().next(".detail").slideToggle();
+	    return false;
+    });
+    $(".nav_toggle").click(function(){
+	    $(this).next('.subnav').slideToggle();
+    });
+ });
+
+}
+
+/*
+     FILE ARCHIVED ON 09:42:42 Apr 24, 2013 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 17:18:32 Jun 28, 2026.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  captures_list: 0.41
+  exclusion.robots: 0.044
+  exclusion.robots.policy: 0.037
+  esindex: 0.007
+  cdx.remote: 54.609
+  LoadShardBlock: 55.445 (3)
+  PetaboxLoader3.datanode: 57.92 (4)
+  PetaboxLoader3.resolve: 73.262 (2)
+  load_resource: 118.446
+*/
